21a996db3034ff2c8f9b2d4b19c6d9e81ae2ac10
hjd919 <297538600@qq.com>
2017-11-02 14:28:08 +0800

    lala

---
+++ app/Console/Commands/Import/ImportEmails.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands\Import;
+
+use App\App;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class ImportEmails extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:emails';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $file = './11021325.csv';
+        $fp   = fopen($file, 'r');
+        $r    = $i    = $j    = 0;
+        while (($data = fgetcsv($fp, 100, ';')) !== false) {
+            list($email, $appleid_password) = $data;
+            if (empty($email) || empty($appleid_password)) {
+                $i++;
+                continue;
+            }
+
+            // 判断文件中是否有重复的udid
+            $exist = DB::table('emails')->where(['email' => $email])->first();
+            if ($exist) {
+                $r++;
+                continue;
+            }
+
+            DB::table('emails')->insert([
+                'email'            => $email,
+                'appleid_password' => $appleid_password,
+                'password'         => $appleid_password,
+                'is_valid'         => 301,
+                'mac'              => $WIFI,
+            ]);
+            $j++;
+        }
+        echo 'good:' . $i . '--bad:' . $j . '--repeat:' . $r;
+        die('1');
+    }
+}
