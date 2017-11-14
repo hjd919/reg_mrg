853e62692c37ec60d6cd886772d95463f80531ad
hjd919 <297538600@qq.com>
2017-11-14 12:21:36 +0800

    lala

--- app/Console/Commands/Task/TaskMobileFail.php
+++ app/Console/Commands/Task/TaskMobileFail.php
@@ -61,24 +61,26 @@ class TaskMobileFail extends Command
         $mobiles = DB::table('mobiles')->where('mobile_group_id', '>', 0)->get();
 
         try {
-            foreach ($mobiles as $mobile) {
+            foreach ($mobiles as $key => $mobile) {
                 // 处理无效手机
                 $this->handleInvalidMobile($mobile);
+
+                echo "处理第" . ($key++) . "条mobiles\n";
             }
         } catch (\Exception $e) {
             $msg = $e->getMessage();
-            
+
             // 邮件警告异常,一小时发一条
-            $tag = explore('|', $msg)[1];
-            $send_email = Redis::get('mobile-fail-send_email:'.$tag);
+            $tag        = explore('|', $msg)[1];
+            $send_email = Redis::get('mobile-fail-send_email:' . $tag);
             if ($send_email) {
                 die('邮件警告异常');
             }
-            Redis::set('mobile-fail-send_email:'.$tag, 1);
-            Redis::expire('mobile-fail-send_email:'.$tag, 7200);
+            Redis::set('mobile-fail-send_email:' . $tag, 1);
+            Redis::expire('mobile-fail-send_email:' . $tag, 7200);
 
-            $toMail  = 'tianlin@xiaozi.com.cn';
-            $cc  = ['297538600@qq.com','huangshimeng@xiaozi.com.cn'];
+            $toMail = 'tianlin@xiaozi.com.cn';
+            $cc     = ['297538600@qq.com', 'huangshimeng@xiaozi.com.cn'];
             Mail::raw($msg, function ($message) use ($toMail, $cc) {
                 $message->subject('机刷邮件警告异常');
                 $message->to($toMail);
@@ -92,17 +94,21 @@ class TaskMobileFail extends Command
     {
         // * 获取手机最后访问时间（一定时间内，没有访问服务器则视为失效数）
         $mobile_access_time = Redis::hGet('mobiles_access_time', $mobile->device_id);
-        
+
         // * 如果超过一定时间（10分钟），失效的手机则获取mobile_group_id=0的手机并更新为对应的mobile_group_id
         if ($this->now_time - $mobile_access_time > 600) {
+            echo '有手机异常' . json_encode([
+                '$this->now_time'     => $this->now_time,
+                '$mobile_access_time' => $mobile_access_time,
+            ]) . "\n";
             // * 获取mobile_group_id=0的手机，如果没有了则退出循环，并邮件警告
             $mgi0 = DB::table('mobiles')->select('id')->where('mobile_group_id', 0)->first();
             if (!$mgi0) {
                 throw new \Exception('no moblie_group_id=0 mobile, mobile_group_id has error mobile|' .
-                json_encode(['mobile_group_id'=>$mobile->mobile_group_id,'mobile_id'=>$mobile->id]));
+                    json_encode(['mobile_group_id' => $mobile->mobile_group_id, 'mobile_id' => $mobile->id]));
             }
 
-            $res = DB::table('mobiles')->where('id', $mgi0->id)->update(['mobile_group_id'=>$mobile->mobile_group_id]);
+            $res = DB::table('mobiles')->where('id', $mgi0->id)->update(['mobile_group_id' => $mobile->mobile_group_id]);
             if (!$res) {
                 throw new \Exception('update mobile mobile_group_id error|');
             }
