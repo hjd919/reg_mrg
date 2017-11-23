919a878583f5a5253bbf79c3bd32e0cf7eaf6681
hjd <297538600@qq.com>
2017-11-23 11:32:46 +0800

    同步server

--- app/Models/WorkDetail.php
+++ app/Models/WorkDetail.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
 
 class WorkDetail extends Model
 {
@@ -14,7 +16,9 @@ class WorkDetail extends Model
     public static function getUsableBrushNum($appid)
     {
         $used_num  = self::where('appid', $appid)->count();
+        Log::error('used_num:' . $used_num);
         $total_num = DB::table('emails')->where('is_valid', 301)->where('valid_status', 1)->count();
+        Log::error('total_num:' . $total_num);
         return $total_num - $used_num;
     }
 
