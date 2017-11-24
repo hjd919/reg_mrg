8002d8e221475229049f93550d735740e5a5966e
hjd <297538600@qq.com>
2017-11-24 18:03:44 +0800

    fdsafas

--- app/Support/Pop3.php
+++ app/Support/Pop3.php
@@ -79,9 +79,17 @@ class Pop3
         Util::log('email', $email);
         Util::log('password', $password);
 
-        $output = system("/usr/local/php/bin/php /home/webapps/jishua_api/pop3.php {$email} {$password} {$comand_url} {$port}");
+	unset($output);
+	unset($output2);
+
+        //$output = system("/usr/local/php/bin/php /home/webapps/jishua_api/pop3.php {$email} {$password} {$comand_url} {$port}");
+        exec("/usr/local/php/bin/php /home/webapps/jishua_api/pop3.php {$email} {$password} {$comand_url} {$port}", $output,$output2);
+        //$output2 = passthru("/usr/local/php/bin/php /home/webapps/jishua_api/pop3.php {$email} {$password} {$comand_url} {$port}", $output);
+
         Util::log('output', $output);
-        return $output;
+        Util::log('output2', $output2);
+        return join("\r\n",$output);
+        return false;
         $curl = curl_init();
         /* Set username and password */
         curl_setopt($curl, CURLOPT_USERNAME, $email);
