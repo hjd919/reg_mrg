bf722ad161d2852b6fb37b3956d86130e67aa685
hjd919 <297538600@qq.com>
2017-11-27 11:45:43 +0800

    bug

--- app/Console/Commands/CronTask/MarkMobileValid.php
+++ app/Console/Commands/CronTask/MarkMobileValid.php
@@ -6,6 +6,7 @@ use App\App;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Redis;
 
 class MarkMobileValid extends Command
 {
