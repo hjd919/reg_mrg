ca9f42bb1a8c4fb97b41e07e5e1456d8b6edf1d1
hjd919 <297538600@qq.com>
2017-11-27 12:04:45 +0800

    引入必要库

--- app/Http/Controllers/Appleid/TaskController.php
+++ app/Http/Controllers/Appleid/TaskController.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Controller;
 use App\Support\Util;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Redis;
 
 class TaskController extends Controller
