dcc036a7b7982013b0a699b5d54ce2e1d380562a
hjd919 <297538600@qq.com>
2017-12-16 21:30:58 +0800

    再跑一次统计执行次数

--- app/Console/Commands/Data/ToIosApp.php
+++ app/Console/Commands/Data/ToIosApp.php
@@ -41,35 +41,40 @@ class ToIosApp extends Command
      */
     public function handle()
     {
-        // $appid    = '1211055336';
-        // $key  = "used_account_ids:appid_{$appid}";
+        $appid = '1211055336';
+        $key   = "used_account_ids:appid_{$appid}";
         // // $key = "useful_account_ids:appid_{$appid}";
         // // $key = 'valid_account_ids';
-        // echo Redis::sSize($key);
-        // die;
+        echo Redis::sSize($key) . "\n";
         // 已用过账号
         $appid    = '1211055336';
         $sort_key = "used_account_ids:appid_{$appid}";
         $offset   = 10000;
+        $j        = $i        = 0;
         while (1) {
-            $data = WorkDetail::getWorkDetailTable($appid)->select('account_id')->where('appid', $appid)->offset($offset)->limit(10000)->get();
+            $data = WorkDetail::getWorkDetailTable($appid)->select('account_id')->where('appid', $appid)->groupBy('account_id')->orderBy('id', 'asc')->offset($offset)->limit(10000)->get();
             if ($data->isEmpty()) {
                 break;
             }
             echo 'offset-' . $offset . "\n";
             $offset += 10000;
             foreach ($data as $key => $r) {
+                $i++;
                 $res = Redis::sAdd($sort_key, $r->account_id);
+                if ($res) {
+                    $j++;
+                }
             }
         }
-        echo Redis::sSize($sort_key)."\n";
+        echo Redis::sSize($sort_key) . "\n";
+        echo "执行了多少次--{$i}--{$j}\n";
         // die;
-// diff two sort
+        // diff two sort
         // 某个时间点未用过账号
         $total_key = 'valid_account_ids';
         $sort_key  = "used_account_ids:appid_{$appid}";
         var_dump(Redis::sDiffStore("useful_account_ids:appid_{$appid}", $total_key, $sort_key)) . "\n";
-        echo Redis::sSize("useful_account_ids:appid_{$appid}")."\n";
+        echo Redis::sSize("useful_account_ids:appid_{$appid}") . "\n";
         die;
 
         // 导出并删除失效账号
