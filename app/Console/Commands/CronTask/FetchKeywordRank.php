755238ed258c91477e6a9b502135f88299a0ea6b
hjd919 <297538600@qq.com>
2017-12-23 03:28:05 +0800

    抓取排名更新数据

--- app/Console/Commands/CronTask/FetchKeywordRank.php
+++ app/Console/Commands/CronTask/FetchKeywordRank.php
@@ -2,8 +2,10 @@
 
 namespace App\Console\Commands\CronTask;
 
-use App\App;
+use App\Models\App;
+use App\Support\Util;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class FetchKeywordRank extends Command
 {
@@ -48,21 +50,30 @@ class FetchKeywordRank extends Command
         }
 
         if (!$app_ids) {
+            $start_time  = date('Y-m-d H', strtotime('-1 days'));
+
             // 查询24小时前开始任务的app_ids
-            $app_ids = App::select('id', 'keyword')->where('start_time', '<=', date('Y-m-d H', strtotime('-1 days')))->get();
+            $app_ids = App::select('id', 'keyword')
+                ->where('create_time', '>', date('Y-m-d', strtotime('-1 days')))
+                ->where('create_time', '<', date('Y-m-d'))
+                ->where('start_time', '<=', $start_time)
+                ->where('appid', $appid)
+                ->get();
             if ($app_ids->isEmpty()) {
                 return true;
             }
-            $app_ids = json_encode($app_ids);
+            $app_ids = json_encode($app_ids, JSON_UNESCAPED_UNICODE);
         }
 
         // 获取关键词排名 {[id,after_rank,on_rank_time,on_rank_start,on_rank_end]}
-        exec("casperjs ./casperjs/chandashi.js --appid={$appid} --app_ids={$app_ids}", $result);
-        Util::log('keyword_rank', $keyword_rank);
+        $cmd = "casperjs --web-security=no --cookies-file=./casperjs/cookie.txt ./casperjs/chandashi.js --appid={$appid} --app_ids='{$app_ids}'";
+        Util::log('cmd', $cmd);
+        exec($cmd, $result);
+        Util::log('result', $result);
 
         // 处理结果
-        if (empty($result[0]) || !($keyword_rank = json_decode($result[0]))) {
-
+        if (empty($result[0]) || !($keyword_rank = json_decode($result[0], true))) {
+            echo '获取关键词排名失败';
             // 控制发邮件频率，一分钟一个人只发一封
             $key = 'notify_fail_fetch_rank:email_' . $toMail;
             if (!Redis::get($key)) {
@@ -82,9 +93,30 @@ class FetchKeywordRank extends Command
 
         // 保存结果
         foreach ($keyword_rank as $d) {
-            $id = $d['id'];
-            unset($d['id']);
-            App::where('id', $id)->update($d);
+
+            // 更新榜单记录
+            $app_data = [
+                'on_rank_start' => date('Y-m-d H:i:s', $d['on_rank_start']),
+                'on_rank_end'   => date('Y-m-d H:i:s', $d['on_rank_end']),
+                'after_rank'    => $d['after_rank'],
+                'on_rank_time'  => $d['on_rank_time'],
+            ];
+            App::where('id', $d['id'])->update($app_data);
+
+            // 记录每小时榜单记录
+            DB::table('app_ranks')->where('app_id',$d['id'])->delete();
+            $rank_data = $d['rank_data'];
+            $app_ranks = [];
+            foreach ($rank_data as $r_d) {
+                $app_ranks[] = [
+                    'app_id'  => $d['id'],
+                    'appid'   => $appid,
+                    'keyword' => $d['keyword'],
+                    'rank'    => $r_d[1],
+                    'time'    => date('Y-m-d H:i:s', $r_d[0] / 1000),
+                ];
+            }
+            DB::table('app_ranks')->insert($app_ranks);
         }
     }
 }
--- casperjs/chandashi.js
+++ casperjs/chandashi.js
@@ -1,9 +1,19 @@
 var utils = require('utils')
 
+Array.prototype.contains = function (obj) {
+    var i = this.length;
+    while (i--) {
+        if (this[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}  
+
 // 获取我们自己的关键词 [{id,keyword,keyword_id}]
 var getOurKeywords = function (keywordData, keyword_data) {
     var our_keywords = keywordData.filter(function (row, index) {
-        return row[0].indexOf(keyword_data) != -1
+        return keyword_data.contains(row[0])
     })
 
     return our_keywords
@@ -16,7 +26,7 @@ var getOurKeywordsLinks = function (appid, keyword_ids) {
     var keyword_id
     var links = keyword_ids.map(function (row) {
         keyword_id = row[5]
-        return 'https://www.chandashi.com/keyword/coverhistorynew/country/cn/appId/' + appid + '/keyword/' + keyword_id + '.html?start=' + start_time + '&end=' + end_time + '&switchtype=day'
+        return 'https://www.chandashi.com/keyword/coverhistorynew/country/cn/appId/' + appid + '/keyword/' + keyword_id + '.html?start=' + start_time + '&end=' + end_time + '&switchtype=hour'
     })
     return links
 }
@@ -43,8 +53,12 @@ var appid = cli.get('appid')
 if (!appid) {
     casper.echo('缺少appid capserjs chandashi.js --appid={appid}').exit();
 }
-// var app_ids = cli.get('app_ids')
-var app_ids = [{ id: 1, keyword: '免流量的游戏' }]
+var app_ids = cli.get('app_ids')
+if (!app_ids) {
+    casper.echo('缺少app_ids capserjs chandashi.js --appid={appid} --app_ids').exit();
+}
+app_ids = JSON.parse(app_ids)
+// var app_ids = [{ id: 3, keyword: '聊天软件' }]
 
 var keyword_list_url = 'https://www.chandashi.com/apps/keywordcover/appId/' + appid + '/country/cn.html'
 
@@ -104,15 +118,33 @@ casper.then(function () {
     var keyword_data = app_ids.map(function (val) {
         return val.keyword
     })
+    // 格式化关键词对应app_id {keyword:app_id}
+    var len = app_ids.length, keyword_ids = {}
+    for (var index = 0; index < len; index++) {
+        keyword_ids[app_ids[index].keyword] = app_ids[index].id;
+    }
 
-    // 获取关键词
+    // 过滤出所需关键词
     var our_keywords = getOurKeywords(keywordData, keyword_data)
 
-    // 获取关键词的历史趋势链接
+    // 获取所需关键词的历史趋势链接
     our_keywords_links = getOurKeywordsLinks(appid, our_keywords)
 
     // 获取关键词的历史趋势
-    var keyword_rank_page, keyword_rank
+    var keyword_rank_page,
+        keyword_rank,
+        keyword_rank_data,
+        keyword,
+        len,
+        point,
+        pont_time,
+        rank,
+        min_rank,
+        min_rank_start,
+        on_rank_time,
+        index,
+        rank_data,
+        min_rank_end
     this.each(our_keywords_links, function (self, link) {
 
         self.thenOpen(link, {
@@ -124,14 +156,57 @@ casper.then(function () {
             // 解析返回的数据 获取关键词趋势数据
             keyword_rank_page = JSON.parse(this.getPageContent())
             keyword_rank_page = keyword_rank_page.data.points[0]
-            keyword_rank = {}
-            keyword_rank[keyword_rank_page.name] = keyword_rank_page.data
+            keyword_rank_data = keyword_rank_page.data
+            
+            keyword = keyword_rank_page.name
+
+            // 分析出在榜时长、上榜开始、结束、现排名
+            // 找出最小的排名
+            len = keyword_rank_data.length
+            min_rank = 10000
+            min_rank_start = 0
+            on_rank_time = 0
+            min_rank_end = 0
+            for (index = 0; index < len; index++) {
+                point = keyword_rank_data[index];
+                point_time = point[0]
+                rank = point[1]
+
+                if (rank > min_rank) {
+                    continue
+                }
+
+                // 如果有更小的排名
+                if (rank < min_rank) {
+                    min_rank = rank
+                    min_rank_start = point_time / 1000
+                    on_rank_time = 0
+                }
+
+                // 结束时间为下一个小时
+                min_rank_end = keyword_rank_data[index][0] / 1000
+
+                // 在榜时长+1
+                on_rank_time++
+            }
+
+            keyword_rank = {
+                'keyword': keyword,
+                'rank_data': keyword_rank_data,
+                'after_rank': min_rank,
+                'on_rank_time': on_rank_time,
+                'on_rank_start': min_rank_start,
+                'on_rank_end': min_rank_end,
+                'id': keyword_ids[keyword],
+            }
+
             keyword_ranks.push(keyword_rank)
         });
     });
 })
 
 casper.run(function () {
-    utils.dump(keyword_ranks);
+    // { [id, after_rank, on_rank_time, on_rank_start, on_rank_end,ranks] }
+    this.echo(JSON.stringify(keyword_ranks));
     this.exit();
 });
