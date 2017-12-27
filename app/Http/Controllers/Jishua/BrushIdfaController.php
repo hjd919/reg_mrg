3a90d9fa23e1790f321bc1d69340e97a35998129
hjd919 <297538600@qq.com>
2017-12-27 16:07:57 +0800

    reids

--- app/Http/Controllers/Jishua/BrushIdfaController.php
+++ app/Http/Controllers/Jishua/BrushIdfaController.php
@@ -1,9 +1,10 @@
 <?php
 namespace App\Http\Controllers\Jishua;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redis;
 
 class BrushIdfaController extends Controller
 {
