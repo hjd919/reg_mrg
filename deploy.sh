929448871bbddc348fd04aaa7883c1f6c8aabd94
hjd919 <297538600@qq.com>
2018-01-17 11:25:41 +0800

    bugfix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git commit -a -m $1
 git push
-cmd="cd jishua_api && git pull"
+cmd="cd jishua_api && git pull && chown -R nobody:nobody *"
 ssh hjd@60.205.58.24 ${cmd}
 ssh hjd@101.201.28.127 ${cmd}
