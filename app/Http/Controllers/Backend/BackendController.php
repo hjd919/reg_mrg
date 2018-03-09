1bc525c954c961f8caf19bcad9c9f29ee5e95e27
hjd919 <hjd919@126.com>
2018-03-09 11:13:27 +0800

    ooo

---
+++ app/Http/Controllers/Backend/BackendController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class BackendController extends Controller
+{
+    /**
+     * Get the guard to be used during authentication.
+     *
+     * @return \Illuminate\Contracts\Auth\Guard
+     */
+    public function guard()
+    {
+        return Auth::guard();
+    }
+
+    public function where_view($where, $where_type = 1)
+    {
+        $user_id = $this->guard()->user()->id;
+        if ($user_id != 10 && $user_id != 5) {
+            if ($where_type == 1) {
+                $where['user_id'] = $user_id;
+            } else {
+                $where[] = ['user_id', '=', $user_id];
+            }
+        }
+        return $where;
+    }
+}
