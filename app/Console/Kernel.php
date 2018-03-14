1ea626bb6b3bb7b3b2aec5de5561c5995214e388
hjd919 <hjd919@126.com>
2018-03-14 18:23:56 +0800

    aaa

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use App\Console\Commands\CronTask\CopyAppleids;
 use App\Console\Commands\Import\ImportAppleids;
+use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
 
 class Kernel extends ConsoleKernel
