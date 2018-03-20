ab0f31cb9080e5aefe3d9a9c430c14059f610458
hjd919 <hjd919@126.com>
2018-03-20 18:44:31 +0800

    fff

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
-    {
-       $schedule->command('copy:appleids')->cron('0 */1 * * * *');
-    }
+    // protected function schedule(Schedule $schedule)
+    // {
+    //    $schedule->command('copy:appleids')->cron('0 */1 * * * *');
+    // }
 }
