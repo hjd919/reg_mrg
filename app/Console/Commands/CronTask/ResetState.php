55cdfb57cd34f5d7b4ae0e053d43b2387d7868f8
hjd919 <hjd919@126.com>
2018-03-21 10:45:14 +0800

    ok

---
+++ app/Console/Commands/CronTask/ResetState.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands\CronTask;
+
+use App\App;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class ResetState extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'reset:state';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $count  = DB::table('appleids')->where('state', 3)->count();
+        $count2 = DB::table('appleids')->where('state', 0)->count();
+        if ($count <= 2000 && !$count2) {
+            $res = DB::table('appleids')->where('state', 3)->update(['state' => 0]);
+            echo "结果是:" . $res;
+        }
+    }
+}
