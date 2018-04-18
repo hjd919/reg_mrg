21c7213df528eeecc5724f255ab5fd642f1af17a
hjd919 <hjd919@126.com>
2018-04-18 17:29:55 +0800

    aaa

--- app/Http/Controllers/Appleid/TaskController.php
+++ app/Http/Controllers/Appleid/TaskController.php
@@ -245,6 +245,15 @@ class TaskController extends Controller
         // 获取yanbox的imap验证码
         if ($email_host == 'yandex.ru') {
             exec("php ./imap.php {$email} {$password}", $output);
+
+            if (empty($output)) {
+                // 没找到邮件列表
+                return response()->json([
+                    'errno'  => 1,
+                    'errmsg' => 'no',
+                    'code'   => '',
+                ], 555);
+            }
             $res = $output[0];
             if ($res === 'is_feng') {
                 DB::table('appleids')->where('strRegName', $email)->update(['state' => 404]);
@@ -253,15 +262,6 @@ class TaskController extends Controller
                     'errmsg' => 'no',
                     'code'   => '',
                 ], 558);
-            }
-
-            if (empty($res)) {
-                // 没找到邮件列表
-                return response()->json([
-                    'errno'  => 1,
-                    'errmsg' => 'no',
-                    'code'   => '',
-                ], 555);
             }
 
             return response()->json([
